2425ac535283a26a35e83670570c1cb7c8701992
albertoadame02 <trabajosalbertoadame@gmail.com>
2026-05-21 12:23:50 +0200

    Prueba Alberto

---
+++ src/main/java/es/biblio/proyectotocho/presentacion/ejercicio6/Prueba.java
@@ -0,0 +1,14 @@
+package es.biblio.proyectotocho.presentacion.ejercicio6;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario25
+ */
+public class Prueba {
+    
+}
